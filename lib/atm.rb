0cd8d1d1b424ae8b6795c70603174b0fb5d9b89f
munemo <john.chimbani@outlook.com>
2020-08-25 14:48:14 +0200

    We are adding require 'date' to fix date error and adding case statement

--- lib/atm.rb
+++ lib/atm.rb
@@ -10,10 +10,19 @@ class ATM
        @fund = 1000
     end 
     
-    def withdraw (amount)
-        @fund -= amount
-        { status: true, message: 'success', date: '2016-01-30', amount: 35, bills: [20,10,5]}
-        { status: false, message: '[reason for failure e. e. wrong pin]', date: '2016-01-30'}
+    def withdraw (amount, account)
+        
+        case
 
-   end
+         when amount > account.balance
+         return          
+        else
+         @fund -= amount
+         account.balance = account.balance - amount
+         require 'date'
+         { status: true, message: 'success', date: Date.today, amount: amount}
+           
+        end
+      end
+   
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -17,14 +17,14 @@ describe ATM do
 
 
     it 'funds are reduced at withdraw' do
-        subject.withdraw 50 
+        subject.withdraw(50, account) 
         expect(subject.fund).to eq 950 
     end
 
     it 'allow withdraw if the account has enough balance.' do
-        expected_output = { status: true, message: 'success', date: Date.time, amount: 45 }
+
+      require 'date'
+        expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
         expect(subject.withdraw(45, account)).to eq expected_output
-      end
-
-
+    end
 end
