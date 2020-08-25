1b7af3607ebbbd2b1f26ef8d52a9312481035b03
Facundo Osores <facundo@MacBook-Pro-de-Facundo-2.local>
2020-08-25 17:01:41 +0200

    Added 2 private methods and refatoring the code

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,28 +1,32 @@
-class ATM
+class Atm
+  attr_accessor :funds
+  require 'date'
 
+  def initialize
+    @funds = 1000
+  end
 
-    attr_accessor:fund
-    attr_accessor:amount
+  def withdraw(amount, account)
+    case
+    when insufficient_funds_in_account?(amount, account)
+      { status: false, message: 'insufficient funds', date: Date.today }
+      
+    else
+      perform_transaction(amount, account)
+    end
+  end
+
+  private
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
     
+  end
 
-    def initialize
+  def perform_transaction(amount, account)
+    @funds -= amount
 
-       @fund = 1000
-    end 
-    
-    def withdraw (amount, account)
-        
-        case
-
-         when amount > account.balance
-         return          
-        else
-         @fund -= amount
-         account.balance = account.balance - amount
-         require 'date'
-         { status: true, message: 'success', date: Date.today, amount: amount}
-           
-        end
-      end
-   
+    account.balance = account.balance - amount
+    { status: true, message: 'success', date: Date.today, amount: amount }
+  end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,28 +1,29 @@
 require './lib/atm.rb'
 
-describe ATM do
+describe Atm do
 
     let(:account) { instance_double('Account') }
 
     before do
-
       allow(account).to receive(:balance).and_return(100)
-
       allow(account).to receive(:balance=)
     end
-
-    it 'has $50 000 on initialize' do
-        expect(subject.fund).to eq 1000
+    
+    it 'rejects withdraw if account has insufficient funds' do
+        expected_output = { status: false, message: 'insufficient funds', date: Date.today }
+        expect(subject.withdraw(105, account)).to eq expected_output
     end
 
+    it 'has $1000 on initialize' do
+        expect(subject.funds).to eq 1000
+    end
 
     it 'funds are reduced at withdraw' do
         subject.withdraw(50, account) 
-        expect(subject.fund).to eq 950 
+        expect(subject.funds).to eq 950 
     end
 
     it 'allow withdraw if the account has enough balance.' do
-
       require 'date'
         expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
         expect(subject.withdraw(45, account)).to eq expected_output
