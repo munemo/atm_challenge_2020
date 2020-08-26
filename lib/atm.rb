03750981fe3faa5e324a9b6173576d79cc1a9f3f
munemo <john.chimbani@outlook.com>
2020-08-26 09:41:14 +0200

    Avilable funds check in ATM is working and all tests are passing

--- lib/atm.rb
+++ lib/atm.rb
@@ -11,12 +11,18 @@ class Atm
     when insufficient_funds_in_account?(amount, account)
       { status: false, message: 'insufficient funds', date: Date.today }
       
+    when insufficient_funds_in_atm?(amount)
+      { status: false, message: 'insufficient funds in ATM', date: Date.today }
     else
       perform_transaction(amount, account)
     end
   end
 
   private
+
+  def insufficient_funds_in_atm?(amount)
+   @funds < amount
+  end
 
   def insufficient_funds_in_account?(amount, account)
     amount > account.balance
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,6 +2,8 @@ require './lib/atm.rb'
 
 describe Atm do
 
+    require 'date'
+
     let(:account) { instance_double('Account') }
 
     before do
@@ -14,6 +16,13 @@ describe Atm do
         expect(subject.withdraw(105, account)).to eq expected_output
     end
 
+    it 'reject withdraw if ATM has insufficient funds' do
+      subject.funds = 50
+        expected_output = { status: false , message: 'insufficient funds in ATM', date: Date.today}
+        expect(subject.withdraw(100, account)).to eq expected_output
+    end
+
+
     it 'has $1000 on initialize' do
         expect(subject.funds).to eq 1000
     end
@@ -24,8 +33,10 @@ describe Atm do
     end
 
     it 'allow withdraw if the account has enough balance.' do
-      require 'date'
+      
         expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
         expect(subject.withdraw(45, account)).to eq expected_output
     end
+    
+
 end
